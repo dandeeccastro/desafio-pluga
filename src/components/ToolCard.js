9def58523efdf1fcfe9978c82ba4a43fccccf6f3
Danilo Collares <dundeepessoal@gmail.com>
2022-03-28 14:05:03 -0300

    🎨 reestruturando cards para serem mais compactos

--- src/components/ToolCard.js
+++ src/components/ToolCard.js
@@ -1,15 +1,12 @@
 import './ToolCard.css';
 
 import Card from "react-bootstrap/Card";
-import Image from "react-bootstrap/Image";
 
 function ToolCard(props) {
   return (
     <Card>
-      <Image style={{ padding:"2em", backgroundColor:props.color }} src={props.icon}/>
-      <Card.Body> 
-        <Card.Title>{props.name}</Card.Title>
-      </Card.Body>
+      <Card.Img variant="top" style={{ padding:"1em", backgroundColor:props.color }} src={props.icon}/>
+      <Card.Header style={{ overflow:"hidden", whiteSpace:"nowrap", textOverflow:"ellipsis"}}>{props.name}</Card.Header>
     </Card>
   );
 }
